31ded22c39b955a96513947a6b90f89d862cc6cc
Numbers00 <kylemariangelo@gmail.com>
2024-01-27 16:01:16 +0800

    tcr: tests pass

--- exer1/src/prices_typed.ts
+++ exer1/src/prices_typed.ts
@@ -78,7 +78,7 @@ function createApp(database: Database) {
   }
 
   function isMonday(date: Date) {
-    return date.getDay() === 1;
+    return convert(date)?.dayOfWeek === 1;
   }
 
   function isHoliday(date: Temporal.PlainDate | undefined) {
