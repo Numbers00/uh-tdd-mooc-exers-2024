9ecd58f7c4ae6a3a22146eff78828403c839a03d
Numbers00 <kylemariangelo@gmail.com>
2024-01-28 12:12:54 +0800

    tcr: tests pass

--- exer1/src/prices_typed.ts
+++ exer1/src/prices_typed.ts
@@ -25,8 +25,7 @@ function createApp(database: Database) {
     res.json({ cost });
   });
 
-  function parseDate(dateString: string | undefined): Date | undefined {
-  }
+
 
   function parsePlainDate(dateString: string | undefined): Temporal.PlainDate | undefined {
     if (!dateString) return;
