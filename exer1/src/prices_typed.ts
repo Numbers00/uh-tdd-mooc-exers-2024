fc72f85c0ad651c7dd61b0caed7e00e7c2106b0c
Numbers00 <kylemariangelo@gmail.com>
2024-01-28 12:06:43 +0800

    tcr: tests pass

--- exer1/src/prices_typed.ts
+++ exer1/src/prices_typed.ts
@@ -77,7 +77,7 @@ function createApp(database: Database) {
 
   function calculateReduction(date: Date | undefined, date2: Temporal.PlainDate | undefined) {
     let reduction = 0;
-    if (date && isMonday(date, date2) && !isHoliday(date, date2)) {
+    if (date && isMonday(date, date2) && !isHoliday(date2)) {
       reduction = 35;
     }
     return reduction;
@@ -87,7 +87,7 @@ function createApp(database: Database) {
     return date.getDay() === 1 && date2?.dayOfWeek === 1;
   }
 
-  function isHoliday(date: Date | undefined, date2: Temporal.PlainDate | undefined) {
+  function isHoliday(date2: Temporal.PlainDate | undefined) {
     const holidays = database.getHolidays();
     for (let row of holidays) {
       let holiday = Temporal.PlainDate.from(row.holiday);
