f41b4b8a1d4e3884de90c2e387d99f860ec35e7e
Numbers00 <kylemariangelo@gmail.com>
2024-01-28 12:13:04 +0800

    tcr: tests pass

--- exer1/src/prices_typed.ts
+++ exer1/src/prices_typed.ts
@@ -20,12 +20,12 @@ function createApp(database: Database) {
     const age = req.query.age ? parseInt(req.query.age as string) : undefined;
     const type = req.query.type as string;
     const baseCost = database.findBasePriceByType(type)!.cost;
-    const date = parsePlainDate(req.query.date as string);
+    const date = parseDate(req.query.date as string);
     const cost = calculateCost(age, type, date, baseCost);
     res.json({ cost });
   });
 
-  function parsePlainDate(dateString: string | undefined): Temporal.PlainDate | undefined {
+  function parseDate(dateString: string | undefined): Temporal.PlainDate | undefined {
     if (!dateString) return;
     return Temporal.PlainDate.from(dateString);
   }
