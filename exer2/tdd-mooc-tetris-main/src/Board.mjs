54c3f3f29ef5f3a238199d9ee94a2eac44ef56a3
Numbers00 <kylemariangelo@gmail.com>
2024-02-03 20:57:50 +0800

    feat: set shapeHasFallen based on whether the block has hit the bottom and tick was run again

--- exer2/tdd-mooc-tetris-main/src/Board.mjs
+++ exer2/tdd-mooc-tetris-main/src/Board.mjs
@@ -2,6 +2,7 @@ export class Board {
   width;
   height;
   boardState = [];
+  shapeHasFallen = false;
 
   constructor(width, height) {
     this.width = width;
@@ -21,11 +22,16 @@ export class Board {
   }
 
   hasFalling() {
-    return true;
+    return !this.shapeHasFallen;
   }
 
   tick() {
     let newBoardState = [...this.boardState];
+
+    if (this.boardState[this.height - 1].some(e => e !== ".")) {
+      this.shapeHasFallen = true;
+      return;
+    }
 
     // Traverse 2D array for board state from bottom up
     // to move X blocks down if they're not already
