2652d0818725973e36d23311ee8a069e27e752e8
Numbers00 <kylemariangelo@gmail.com>
2024-04-21 15:05:07 +0800

    test: add test for falling tetrominoes can be moved left
    Signed-off-by: Numbers00 <kylemariangelo@gmail.com>

--- exer2/tdd-mooc-tetris-main/test/MovingTetrominoes.test.mjs
+++ exer2/tdd-mooc-tetris-main/test/MovingTetrominoes.test.mjs
@@ -1,11 +1,27 @@
-import { beforeEach, describe } from "vitest";
+import { expect } from "chai";
+import { beforeEach, describe, test } from "vitest";
+
 import { Board } from "../src/Board.mjs";
 import { Tetromino } from "../src/Tetromino.mjs";
 
 describe("Falling tetrominoes", () => {
   let board;
+  let shape = Tetromino.T_SHAPE;
+
   beforeEach(() => {
     board = new Board(10, 6);
-    board.drop(Tetromino.T_SHAPE);
+    board.drop(shape);
   });
+
+  test.skip("can be moved left", () => {
+    shape.moveLeft();
+    expect(board.toString()).to.equalShape(
+      `...T......
+       ..TTT.....
+       ..........
+       ..........
+       ..........
+       ..........`
+    );
+  })
 });
